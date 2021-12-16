5621b546a86a472d0e98b610b7ebf2b58ec40ba9
Matteo Pompei <datxizzan@gmail.com>
2021-12-16 15:41:38 +0100

    Secondo esercizio fatto

--- jsnack2/script.js
+++ jsnack2/script.js
@@ -6,9 +6,12 @@
 
 */
 
-let n = prompt("Inserisci un numero");
+let n = parseInt(prompt("Inserisci un numero"));
 
 for (let i = 0; i < n; i++) {
-  let myArray = [4, 8, 15, 16, 23, 42];
-  document.writeln("<h1>" + myArray + "</h1>");
+  let myArray = [];
+  for (let i2 = 0; i2 < 10; i2++) {
+    myArray.push(Math.floor(Math.random() * 100) +1);
+  }
+  document.writeln("<div>" + myArray + "</div>");
 }
