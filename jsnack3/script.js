1217e31f78766c10a9684062750d9a48c4705551
Matteo Pompei <datxizzan@gmail.com>
2021-12-16 16:26:54 +0100

    Esercizio 3: div creati

--- jsnack3/script.js
+++ jsnack3/script.js
@@ -2,6 +2,19 @@
 
   Crea due tag div con due id diversi:
   un div avrà il testo colorato di rosso mentre l’altro di verde.
-  Partendo da un array di numeri, stampiamo nell’id rosso i numeri dispari e in verde i numeri pari.
+  Partendo da un array di numeri, stampiamo nell’id rosso
+  i numeri dispari e in verde i numeri pari.
 
 */
+
+// Div Rosso
+let divRed = document.createElement("div");
+divRed.setAttribute("id", "red");
+divRed.innerHTML = "Emblema";
+document.body.appendChild(divRed);
+
+// Div verde
+let divGreen = document.createElement("div");
+divGreen.setAttribute("id", "green");
+divGreen.innerHTML = "Problema";
+document.body.appendChild(divGreen);
