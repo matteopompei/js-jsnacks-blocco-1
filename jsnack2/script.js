6cc430ee2964a48948d9786e419965a25db85add
Matteo Pompei <datxizzan@gmail.com>
2021-12-16 15:34:42 +0100

    Abbozzo secondo esercizio

--- jsnack2/script.js
+++ jsnack2/script.js
@@ -5,3 +5,10 @@
   Ogni volta che ne crei uno, stampalo a schermo.
 
 */
+
+let n = prompt("Inserisci un numero");
+
+for (let i = 0; i < n; i++) {
+  let myArray = [4, 8, 15, 16, 23, 42];
+  document.writeln("<h1>" + myArray + "</h1>");
+}
