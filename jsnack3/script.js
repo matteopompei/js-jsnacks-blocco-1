e5c79b4c1304c501865f3a0d89fc78b38e6a7383
Matteo Pompei <datxizzan@gmail.com>
2021-12-16 16:34:22 +0100

    Esercizio 3 completo

--- jsnack3/script.js
+++ jsnack3/script.js
@@ -10,11 +10,20 @@
 // Div Rosso
 let divRed = document.createElement("div");
 divRed.setAttribute("id", "red");
-divRed.innerHTML = "Emblema";
 document.body.appendChild(divRed);
 
 // Div verde
 let divGreen = document.createElement("div");
 divGreen.setAttribute("id", "green");
-divGreen.innerHTML = "Problema";
 document.body.appendChild(divGreen);
+
+let lostNumbers = [4, 8, 15, 16, 23, 42];
+
+for (i = 0; i < lostNumbers.length; i++) {
+  if (lostNumbers[i] % 2 != 0) {
+    divRed.innerHTML += lostNumbers[i] + " ";
+  }
+  else {
+    divGreen.innerHTML += lostNumbers[i] + " ";
+  }
+}
